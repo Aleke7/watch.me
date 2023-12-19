b2994003eb44b0d67d89c92f5fac1ed6b226cbfe
Aleke7 <aleke7.kz@gmail.com>
2023-12-19 13:47:17 +0600

    changes

--- watch.me/Scenes/Home/HomeAssembler.swift
+++ watch.me/Scenes/Home/HomeAssembler.swift
@@ -12,7 +12,6 @@ final class HomeAssembler {
         interactor.presenter = presenter
         presenter.viewController = viewController
         router.viewController = viewController
-        router.dataStore = interactor
         
         return viewController
     }
